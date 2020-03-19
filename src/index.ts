482c289e3b4a34c43ca184b16e8a979adb372052
jaehunkim <jaehunkim7730@gmail.com>
2020-03-19 18:52:32 +0900

    Add drawing library typing

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,399 @@ export interface IKakaoMaps {
   Rectangle: new (options?: IKakaoRectangleOptions) => IKakaoRectangle;
   Ellipse: new (options?: IKakaoEllipseOptions) => IKakaoEllipse;
   event: IKakaoEvent;
+  drawing: IKakaoDrawing;
   MapTypeId: IKakaoMapTypeId;
   ControlPosition: IKakaoControlPosition;
   CopyrightPosition: IKakaoCopyrightPosition;
+}
+
+export interface IKakaoDrawing {
+  OverlayType: IKakaoDrawingOverayType;
+  Toolbox: new (options: IKakaoDrawingToolboxOptions) => IKakaoDrawingToolbox;
+  DrawingManager: new (options?: IDrawingManagerOptions) => IKakaoDrawingManager;
+}
+
+export interface IKakaoDrawingToolboxOptions {
+  /** 그리기 모드를 가져올 DrawingManager 객체 */
+  drawingManager: IKakaoDrawingManager;
+}
+
+export interface IKakaoDrawingToolbox {
+  getElement(): Element;
+}
+
+export interface IDrawingManagerOptions {
+  /** 마커와 그리기 요소를 그릴 지도 객체 */
+  map: IKakaoMap;
+
+  /**
+   * 마우스 오버 시 가이드 툴팁 표시 여부.
+   *
+   * ‘draw’, ‘drag’, ‘edit’ 3가지를 지정할 수 있다. (기본값: 모두 표시 안함)
+   *
+   * 예를들어 [‘draw’]로 설정하면 객체를 그릴때 가이드 툴팁이 표시된다.
+   * */
+  guideTooltip?: string[];
+
+  /** 사용할 그리기 요소 지정한다 (기본값: 모든 그리기 요소) */
+  drawingMode?: (IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType])[];
+
+  /** 마커 그리기 옵션 */
+  markerOptions?: {
+    /** 드래그 가능한 마커 */
+    draggable?: boolean;
+
+    /** 삭제가 가능한 마커 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+    removable?: boolean;
+
+    /** 그릴 마커의 이미지를 설정한다 (기본값: API 기본 마커 이미지) */
+    markerImages?: IDrawingMarkerImageOptions[];
+  };
+
+  /** 사각형 그리기 옵션 */
+  rectangleOptions?: IDrawingRectangleOptions;
+
+  /** 원 그리기 옵션 */
+  circleOptions?: IDrawingCircleOptions;
+
+  /** 타원 그리기 옵션 */
+  ellipseOptions?: IDrawingEllipseOptions;
+
+  /** 선 그리기 옵션 */
+  polylineOptions?: IDrawingPolylineOptions;
+
+  /** 화살표 선 그리기 옵션 */
+  arrowOptions?: IDrawingArrowOptions;
+
+  /** 다각형 그리기 옵션 */
+  polygonOptions?: IDrawingPolygonOptions;
+}
+
+export interface IDrawingMarkerImageOptions {
+  /** 마커 이미지 URL */
+  src: string;
+
+  /** 마커의 너비 */
+  width: number;
+
+  /** 마커의 높이 */
+  height: number;
+
+  /** 마커의 좌표에 일치시킬 이미지 안의 X 좌표 */
+  offsetX?: number;
+
+  /** 마커의 좌표에 일치시킬 이미지 안의 Y 좌표 */
+  offsetY?: number;
+
+  /** 스프라이트 이미지의 전체 너비 */
+  spriteWidth?: number;
+
+  /** 스프라이트 이미지의 전체 높이 */
+  spriteHeight?: number;
+
+  /** 스프라이트 이미지에서 사용할 영역의 좌측 좌표 */
+  spriteOriginX?: number;
+
+  /** 스프라이트 이미지에서 사용할 영역의 상단 좌표 */
+  spriteOriginY?: number;
+
+  /** 마커의 클릭 또는 마우스오버 가능한 영역의 모양 */
+  shape?: string;
+
+  /** 마커의 클릭 또는 마우스오버 가능한 영역을 표현하는 좌표값 */
+  coords?: string;
+
+  hoverImage?: IDrawingMarkerImageOptions;
+  dragImage?: IDrawingMarkerImageOptions;
+}
+
+export interface IDrawingRectangleOptions {
+  /** 드래그 가능한 사각형 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 사각형 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 사각형 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 외각선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 외곽선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 외곽선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 외곽선선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** #xxxxxx 형태의 채우기 색 (기본값: ‘#F10000’) */
+  fillColor?: string;
+
+  /** 채우기 불토명도 (0-1, 기본값: 0) */
+  fillOpacity?: number;
+}
+
+export interface IDrawingCircleOptions {
+  /** 드래그 가능한 원 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 원 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 원 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 외각선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 외곽선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 외곽선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 외곽선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** #xxxxxx 형태의 채우기 색 (기본값: ‘#F10000’) */
+  fillColor?: string;
+
+  /** 채우기 불토명도 (0-1, 기본값: 0) */
+  fillOpacity?: number;
+}
+
+export interface IDrawingEllipseOptions {
+  /** 드래그 가능한 타원 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 타원 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 타원 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 외각선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 외곽선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 외곽선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 외곽선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** #xxxxxx 형태의 채우기 색 (기본값: ‘#F10000’) */
+  fillColor?: string;
+
+  /** 채우기 불토명도 (0-1, 기본값: 0) */
+  fillOpacity?: number;
+}
+
+export interface IDrawingPolylineOptions {
+  /** 드래그 가능한 선 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 선 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 선 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** 그리기 중, 마우스를 따라다니는 보조선의 스타일 (기본값: ‘solid’) */
+  hintStrokeStyle?: string;
+
+  /** 그리기 중, 마우스를 따라다니는 보조선의 투명도. (기본값: 0.5) */
+  hintStrokeOpacity?: number;
+}
+
+export interface IDrawingArrowOptions {
+  /** 드래그 가능한 화살표 선 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 화살표 선 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 화살표 선 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 화살표 선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 화살표 선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 화살표 선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 화살표 선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** 그리기 중, 마우스를 따라다니는 보조선의 스타일 (기본값: ‘solid’) */
+  hintStrokeStyle?: string;
+
+  /** 그리기 중, 마우스를 따라다니는 보조선의 투명도. (기본값: 0.5) */
+  hintStrokeOpacity?: number;
+
+  /** 시작점의 화살표 표시 여부 (기본값: false) */
+  startArrow?: boolean;
+}
+
+export interface IDrawingPolygonOptions {
+  /** 드래그 가능한 다각형 */
+  draggable?: boolean;
+
+  /** 삭제 가능한 다각형 (true로 설정하면 마우스 오버 시 삭제할 수 있는 X 버튼이 표시된다) */
+  removable?: boolean;
+
+  /** 수정 가능한 다각형 (true로 설정하면 마우스 오버 시 수정할 수 있는 작은 사각형이 표시된다) */
+  editable?: boolean;
+
+  /** 외각선의 두께 (기본값: 3) */
+  strokeWeight?: number;
+
+  /** #xxxxxx 형태의 외곽선 색 (기본값: ‘#F10000’) */
+  strokeColor?: string;
+
+  /** 외곽선의 불투명도 (0-1, 기본값: 1.0) */
+  strokeOpacity?: number;
+
+  /** 외곽선 스타일 (기본값: ‘solid’) */
+  strokeStyle?: string;
+
+  /** #xxxxxx 형태의 채우기 색 (기본값: ‘#F10000’) */
+  fillColor?: string;
+
+  /** 채우기 불토명도 (0-1, 기본값: 0) */
+  fillOpacity?: number;
+}
+
+/**
+ * 그리기 관리자 객체를 생성한다.
+ *
+ * {@link http://apis.map.kakao.com/web/sample/basicDrawingLibrary drawing Library 사용하기 샘플보기}
+ *
+ * {@link http://apis.map.kakao.com/web/sample/drawingGetData drawing Library에서 데이터 얻기 샘플보기}
+ */
+export interface IKakaoDrawingManager {
+  setStyle: (type: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType], props: string, value: string | number) => void;
+  setStrokeWeight: (strokeWeight: number) => void;
+
+  /** format: #xxxxxx */
+  setStrokeColor: (strokeColor: string) => void;
+
+  /**
+   * 그리기 요소를 선택한다. 이미 선택한 상태에서 다시 호출하면 이 전 선택된 요소는 취소된다.
+   *
+   * 여러개의 마커 이미지를 선언했을 경우, 두 번째 파라메터로 해당 마커 이미지의 인덱스 값을 넣는다.
+   *
+   * 인덱스 값은 0 부터 시작한다.
+   */
+  select: (type: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType], index?: number) => void;
+  cancel: () => void;
+
+  /**
+   * 그리기 요소를 이전 상태로 되돌린다.
+   *
+   * 그리기 요소의 생성/수정/이동/삭제 동작을 되돌릴 수 있고 여러번 호출이 가능하다.
+   *
+   * 그리기 요소의 상태는 최대 20개 까지 저장된다.
+   */
+  undo: () => void;
+
+  /**
+   * 이전 상태로 되돌린 상태를 취소한다.
+   *
+   * 여러번의 undo 호출 이후에 DrawingManager 객체에 그리기 요소에 대한 새로운 상태가 추가되지 않으면 되돌리기 이전 상태까지 실행할 수 있다.
+   */
+  redo: () => void;
+
+  /** 그리기 요소를 이전 상태로 되돌릴 수 있는지 여부를 반환한다. */
+  undoable: () => boolean;
+
+  /** 이전으로 되돌린 상태를 취소할 수 있는지 여부를 반환한다. */
+  redoable: () => boolean;
+
+  /**
+   * 지도에 그려진 요소들 중 선택한 요소의 데이터를 반환한다.
+   *
+   * 요소를 지정하지 않으면 그려진 모든 요소의 데이터를 반환한다.
+   *
+   * {@link http://apis.map.kakao.com/web/sample/drawingGetData drawing Library에서 데이터 얻기 샘플보기}
+   */
+  getData: (types?: (IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType])[]) => { [shape: string]: any }
+
+  /**
+   * 지도에 그려진 요소들 중 선택한 요소의 객체를 반환한다.
+   *
+   * 요소를 지정하지 않으면 그려진 모든 요소의 객체를 반환한다.
+   */
+  getOverlays: (types?: (IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType])[]) => { [shape: string]: any }
+
+  /**
+   * overlayType OverlayType : 그리기 관리자에 추가할 오버레이 타입
+   *
+   * param1 : 추가할 그리기 요소가 마커/원인 경우 LatLng를, 타원/사각형인 경우는 LatLngBounds를, 선, 화살표선, 다각형인 경우는 좌표의 배열 또는 좌표 배열의 배열을 받는다.
+   *
+   * param2 : 추가할 그리기 요소가 마커인경우 사용할 마커 이미지의 인덱스를, 원인 경우 원의 반지름을 파라미터로 받는다.
+   */
+  put: (overlayType: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType], param1: IKakaoLatLng | IKakaoLatLngBounds | IKakaoLatLng[] | IKakaoLatLng[][], param2?: number) => void;
+
+  /** overlay : 그리기 관리자에서 생성한 확장 오버레이 객체 */
+  remove: (overlay: any) => void;
+
+  addListener(type: "select", callback: (e: { overlayType: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType] }) => void): void;
+  addListener(type: "drawstart", callback: (mouseEvent: IKakaoDrawingMouseEvent) => void): void;
+  addListener(type: "draw", callback: (mouseEvent: IKakaoDrawingMouseEvent) => void): void;
+  addListener(type: "drawend", callback: (mouseEvent: IKakaoDrawingMouseEvent) => void): void;
+
+  /** 다음 단계 그리기를 하면 발생한다. (Polyline, Polygon, Arrow 한정) */
+  addListener(type: "drawnext", callback: (mouseEvent: IKakaoDrawingMouseEvent) => void): void;
+
+  addListener(type: "cancel", callback: (e: { overlayType: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType] }) => void): void;
+  addListener(type: "remove", callback: (e: { overlayType: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType] }) => void): void;
+
+  /**
+   * 그리기 요소들의 상태가 변경되면 발생한다.
+   *
+   * 각 요소의 생성/수정/이동/삭제 동작과 undo 또는 redo 함수 호출이 이에 해당한다.
+   */
+  addListener(type: "state_changed", callback: () => void): void;
+}
+
+export interface IKakaoDrawingMouseEvent {
+  overlayType: IKakaoDrawingOverayType[keyof IKakaoDrawingOverayType];
+  coords: IKakaoCoords;
+  point: IKakaoPoint;
+
+  /** 그리기 요소의 인스턴스. drawend 이벤트에서만 존재한다. */
+  target: object;
+}
+
+export interface IKakaoDrawingOverayType {
+  MARKER: "marker";
+  RECTANGLE: "rectangle";
+  CIRCLE: "circle";
+  ELLIPSE: "ellipse";
+  POLYLINE: "polyline";
+  POLYGON: "polygon";
+  ARROW: "arrow";
 }
 
 export interface IKakaoViewpoint {
@@ -93,6 +483,7 @@ export interface IKakaoEvent {
 export interface IKakaoMouseEvent {
   latLng: IKakaoLatLng;
   point: IKakaoPoint;
+  coords: IKakaoCoords;
 }
 
 export type TKakaoEventTarget = IKakaoMap | IKakaoRoadview | IKakaoMarker | IKakaoPolyline | IKakaoPolygon | IKakaoCircle | IKakaoEllipse | IKakaoRectangle;
@@ -278,7 +669,7 @@ export interface IKakaoMap {
   setMaxLevel: (maxLevel: number) => void;
   panBy: (dx: number, dy: number) => void;
   panTo: (latlng_or_bounds: IKakaoLatLng | IKakaoLatLngBounds, padding?: number) => void;
-  addControl: (control: IKakaoMapTypeControl | IKakaoMapZoomControl, position?: IKakaoControlPosition[keyof IKakaoControlPosition]) => void;
+  addControl: (control: Element | IKakaoMapTypeControl | IKakaoMapZoomControl, position?: IKakaoControlPosition[keyof IKakaoControlPosition]) => void;
   removeControl: (control: IKakaoMapTypeControl | IKakaoMapZoomControl) => void;
   setDraggable: (draggable: boolean) => void;
   getDraggable: () => boolean;
